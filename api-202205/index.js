1a84005eaf6c91a9b314fb5af3cbd780eb67fc61
Joseph C Wang <joequant@gmail.com>
2022-05-26 11:15:53 +0800

    change endpoints

--- api-202205/index.js
+++ api-202205/index.js
@@ -7,14 +7,16 @@
 const { ApiAdapter, echoFunc, stub1Func } =
       require('./api_adapter')
 
+// note that the api endpoints are for testing purposes onlu and are
+// subject to change
 const app = new ApiAdapter({
   urlPost: {
     'nft-index': 'https://truflation-dev-8080.hydrogenx.tk/nft-calc/index-value'
   },
   urlGet: {
-    'truflation/current': 'https://api.truflation.com/current',
-    'truflation/at-date': 'https://api.truflation.com/at-date',
-    'truflation/range': 'https://api.truflation.com/range'
+    'truflation/current': 'https://truflation-api.hydrogenx.tk/current',
+    'truflation/at-date': 'https://truflation-api.hydrogenx.tk/at-date',
+    'truflation/range': 'https://truflation-api.hydrogenx.tk/range'
   },
   urlEncodeData: {
     'truflation/at-date': true,
