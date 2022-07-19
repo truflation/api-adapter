bf7cd2da43d1d202be72b954c01cc00ee71a2bd3
Joseph C Wang <joequant@gmail.com>
2022-07-19 17:07:27 +0800

    add defilama test

---
+++ api-202205/test-defilama.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -v
+URL_ADAPTER=${URL_ADAPTER=http://localhost:8081/}
+
+curl -X POST -H 'Content-Type: application/json' -i $URL_ADAPTER --data '{"service": "defilama/tvl",
+"data": {"service" : "chains"},
+"abi": "json"
+}'
+
+
+curl -X POST -H 'Content-Type: application/json' -i $URL_ADAPTER --data '{"service": "nuon/dynamic-index",
+"data": {"date" : "2022-07-05"},
+"abi": "json"
+}'
