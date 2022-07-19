0cd2139a5f286cd0e04f630e1804fe20f4ebac6c
Joseph C Wang <joequant@gmail.com>
2022-07-19 17:52:27 +0800

    add defilama.js

---
+++ api-202205/defilama.js
@@ -0,0 +1,48 @@
+// Spdx-License-Identifier: MIT
+// Copyright 2022 - Laguna Labs
+//
+
+const _ = require('lodash');
+
+function defilama_transform(url, data) {
+  method = "get"
+  if (data?.service === undefined) {
+    return [url, data]
+  }
+  if (data.service === "prices") {
+    method = "post"
+  }
+  url += "/" + data.service
+  delete data.service
+  console.log(url, data, method)
+  return [url, data, method]
+}
+
+const defilama_services = {
+  urlGet: {
+    'defilama/tvl': 'https://api.llama.fi',
+    'defilama/coins': 'https://coins.llama.fi',
+    'defilama/stablecoins': 'https//stablecoins.llama.fi',
+    'defilama/yields': 'https://yields.llama.fi'
+  },
+  urlTransform: {
+    'defilama/tvl':  defilama_transform,
+    'defilama/coins': defilama_transform,
+    'defilama/stablecoins': defilama_transform,
+    'defilama/yields': defilama_transform    
+  },
+  urlEncodeData: {
+    'defilama/tvl': true,
+    'defilama/coins': true,
+    'defilama/stablecoins': true,
+    'defilama/yields': true
+  }
+}
+
+function add_defilama(services) {
+  return _.merge(services, defilama_services)
+}
+
+module.exports = {
+  add_defilama
+}
