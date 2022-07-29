7d6ab29d0edf5a02568a170555f9f39f4816d1ec
Joseph C Wang <joequant@gmail.com>
2022-07-30 03:32:17 +0800

    move more tests to mocha

--- test.sh
+++ test.sh
@@ -2,23 +2,6 @@
 set -v
 URL_ADAPTER=${URL_ADAPTER=http://localhost:8082/}
 
-curl -X POST -H 'Content-Type: application/json' -i $URL_ADAPTER --data '{"service": "bad service",
-"data": {"foo": [30, 10530, "string"]},
-"abi": "ipfs"
-}'
-
-
-curl -X POST -H 'Content-Type: application/json' -i $URL_ADAPTER --data '{"service": "echo",
-"data": {"foo": [30, 10530, "string"]},
-"abi": "ipfs"
-}'
-
-curl -X POST -H 'Content-Type: application/json' -i $URL_ADAPTER --data '{"service": "truflation/at-date",
-"data": {"date" : "2021-10-10"},
-"abi": "json"
-}'
-
-
 curl -X POST -H 'Content-Type: application/json' -i $URL_ADAPTER --data '{"service": "truflation/range",
 "data": {"start-date" : "2021-10-10", "end-date": "2021-10-25", "interval": "day"},
 "abi": "ipfs"
--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,30 @@
 #!/usr/bin/env node
 import { ApiAdapter } from '../api_adapter'
+process.env.TRUFLATION_API_HOST='https://truflation-api.hydrogenx.tk'
 import { services, randomized_services } from '../index'
+import axios from 'axios'
+import assert from 'assert'
+
 require('dotenv').config()
 
 const app = new ApiAdapter(services)
+const url = process.env.URL_ADAPTER || 'http://localhost:8081/'
+
+function test_packet(packet, response) {
+  return async() => {
+    const {data, status} = await axios.post(
+      url,
+      packet,
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    if (response !== undefined) {
+      assert.deepEqual(data, response)
+    }
+  }
+}
 
 describe('Test', () => {
   before(async () => {
@@ -12,4 +33,25 @@ describe('Test', () => {
   after(async () => {
     app.close()
   })
+  it('bad service', test_packet({
+    "service": "bad service",
+    "data": {"foo": [30, 10530, "string"]},
+    "abi": "ipfs"
+  }, "no service"))
+  it('connect', test_packet({
+      service: "echo",
+      data: {"foo": [30, 10530, "string"]},
+      abi: "ipfs"
+  }, "QmPuQCKrxf6CUwdB4mUghgR9qKZ89DV5Frrjg5ZPAYyvQF"))
+  it('inflation', test_packet({
+    "service": "truflation/at-date",
+    "data": {"date" : "2021-10-10"},
+    "abi": "json"
+  }, {
+  currentInflationIndex: 136.82006247089433,
+  date: '2021-10-10',
+  yearAgoDate: '2020-10-10',
+  yearAgoInflationIndex: 127.2394421757194,
+  yearOverYearInflation: 7.529599416149551
+  }))
 })
