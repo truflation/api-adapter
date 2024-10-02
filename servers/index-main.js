1be4e8291f2ee465cf6eb4d1c4e5992f91a23f85
williamrusdyputra <williamrusdyputra@gmail.com>
2024-10-02 15:28:29 +0700

    fix: sentry initialization step

--- servers/index-main.js
+++ servers/index-main.js
@@ -4,18 +4,13 @@
 // This is a simple chainlab adapter that processes incoming json
 // packages and outputs json.
 
-const { ApiAdapter } = require('../api_adapter')
-const { DefiLlamaAdapter } = require('../handlers/defillama')
-const { UtilHttpAdapter } = require('../handlers/util_http')
-const { registerUtil } = require('../handlers/util')
-const { services, randomizedServices } = require('../services')
-
+const Sentry = require('@sentry/node')
 require('dotenv').config()
 
 // initialize Sentry for monitoring and logging
-if (process.env.SENTRY_DSN) {
-  const Sentry = require('@sentry/node')
-  const { nodeProfilingIntegration } = require('@sentry/profiling-node')
+const isSentryEnabled = !!process.env.SENTRY_DSN
+const { nodeProfilingIntegration } = require('@sentry/profiling-node')
+if (isSentryEnabled) {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     integrations: [
@@ -26,15 +21,20 @@ if (process.env.SENTRY_DSN) {
   })
 }
 
+const { ApiAdapter } = require('../api_adapter')
+const { DefiLlamaAdapter } = require('../handlers/defillama')
+const { UtilHttpAdapter } = require('../handlers/util_http')
+const { registerUtil } = require('../handlers/util')
+const { services, randomizedServices } = require('../services')
+
 // note that the api endpoints are for testing purposes only and are
 // subject to change
-
-const app = new ApiAdapter(services)
+const app = new ApiAdapter(services, isSentryEnabled, Sentry)
 app.register_handler(new DefiLlamaAdapter())
 app.register_handler(new UtilHttpAdapter())
 registerUtil(app)
 
-const randomizedApp = new ApiAdapter(randomizedServices)
+const randomizedApp = new ApiAdapter(randomizedServices, isSentryEnabled, Sentry)
 randomizedApp.register_handler(new DefiLlamaAdapter())
 randomizedApp.register_handler(new UtilHttpAdapter())
 registerUtil(app)
