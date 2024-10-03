5d3d02bbb079c7969a24a713411be44353e50fc6
williamrusdyputra <williamrusdyputra@gmail.com>
2024-10-03 16:09:00 +0700

    fix: refactor to be dry

--- servers/index-minertoken.js
+++ servers/index-minertoken.js
@@ -4,24 +4,10 @@
 // This is a simple chainlab adapter that processes incoming json
 // packages and outputs json.
 
-const Sentry = require('@sentry/node')
-const { nodeProfilingIntegration } = require('@sentry/profiling-node')
+const { Sentry, isSentryEnabled } = require('./sentry')
 const { ApiAdapter } = require('../api_adapter')
 
 require('dotenv').config()
-
-// initialize Sentry for monitoring and logging
-const isSentryEnabled = !!process.env.SENTRY_DSN
-if (isSentryEnabled) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [
-      nodeProfilingIntegration()
-    ],
-    tracesSampleRate: 0.2,
-    profilesSampleRate: 0.2
-  })
-}
 
 const services = {
   urlGet: {
--- servers/index-randomized.js
+++ servers/index-randomized.js
@@ -4,8 +4,7 @@
 // This is a simple chainlab adapter that processes incoming json
 // packages and outputs json.
 
-const Sentry = require('@sentry/node')
-const { nodeProfilingIntegration } = require('@sentry/profiling-node')
+const { Sentry, isSentryEnabled } = require('./sentry')
 const { ApiAdapter } = require('../api_adapter')
 const { DefiLamaAdapter } = require('../handlers/defilama')
 const { UtilHttpAdapter } = require('../handlers/util_http')
@@ -13,19 +12,6 @@ const { registerUtil } = require('../handlers/util')
 const { randomizedServices } = require('../services')
 
 require('dotenv').config()
-
-// initialize Sentry for monitoring and logging
-const isSentryEnabled = !!process.env.SENTRY_DSN
-if (isSentryEnabled) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [
-      nodeProfilingIntegration()
-    ],
-    tracesSampleRate: 0.2,
-    profilesSampleRate: 0.2
-  })
-}
 
 // note that the api endpoints are for testing purposes onlu and are
 // subject to change
